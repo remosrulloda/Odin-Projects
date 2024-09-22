4690c98a677c3794e46a9a406d68f9143d61301d
Remo <remosrulloda@gmail.com>
2024-09-22 09:41:26 -0700

    Linked Lists

---
+++ Linked-Lists/index.js
@@ -0,0 +1,27 @@
+class LinkedList {
+    constructor() {
+
+    }
+
+    append(value) {
+
+    }
+
+    prepend(value) {
+
+    }
+
+    size() {
+
+    }
+
+
+
+};
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = null;
+    }
+};
