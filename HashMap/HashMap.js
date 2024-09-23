ccf2306e849469471452511e18bbdbe193199c69
Remo <remosrulloda@gmail.com>
2024-09-23 08:51:14 -0700

    Hashmap

---
+++ HashMap/HashMap.js
@@ -0,0 +1,17 @@
+class HashMap {
+
+    constructor() {
+
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+};
