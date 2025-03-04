e214d4414c6a76b1a6a2310c11e3b54ac54654cd
Remo Rulloda <remosrulloda@gmail.com>
2025-03-03 23:32:07 -0800

    fix

---
+++ memory-card/src/Api.jsx
@@ -0,0 +1,31 @@
+// For fetching API 
+
+export default async function getData() {
+    const url = 'https://pokeapi.co/api/v2/pokemon/';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+
+        const pokemon = [];
+
+        for (let i = 0; i < 12; i++) {
+            const pokemonUrl = json.results[i].url;
+            const pokemonResponse = await fetch(pokemonUrl);
+            if (!pokemonResponse.ok) {
+                throw new Error(`Failed to fetch deftails for Pokemon ${i + 1}`);
+            }
+            const pokemonData = await pokemonResponse.json();
+            pokemon.push(pokemonData);
+        }
+
+        return pokemon;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -2,8 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import getData from './Api'
+
+function Card({ data }) {
+  return (
+    <div id="card">
+
+    </div>
+  );
+}
 
 function App() {
+  const pokemon = getData();
+
+  console.log(pokemon);
+
   const [count, setCount] = useState(0)
 
   return (
