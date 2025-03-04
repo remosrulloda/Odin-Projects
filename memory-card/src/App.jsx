764b10ceec23f4d0ba58f07f67defe8aa338ed93
Remo Rulloda <remosrulloda@gmail.com>
2025-03-03 23:56:49 -0800

    test

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,46 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import getData from './Api'
 
 function Card({ data }) {
   return (
     <div id="card">
-
+      {data.length > 0 ? <h2>{data[0].name}</h2> : <p>Loading...</p>}
     </div>
   );
 }
 
 function App() {
-  const pokemon = getData();
+  const [pokemon, setPokemon] = useState([]);
 
-  console.log(pokemon);
-
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    async function fetchPokemon() {
+      const data = await getData();
+      if (data) setPokemon(data);
+    }
+    fetchPokemon();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card data={pokemon} />
     </>
   )
 }
